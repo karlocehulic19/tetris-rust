5d637a3c0f5eb79cc699cf6b7aaa997bfaae4ce9
Karlo Cehulic <karlocehlic@gmail.com>
2026-03-22 22:54:32 +0100

    Add basic player struct

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+const SIZE: usize = 10;
+
 fn main() {
-    println!("Hello, world!");
+    let player1 = Player {
+        name: String::from("Joe"),
+        board: [[0; SIZE]; SIZE],
+    };
+    let player2 = Player {
+        name: String::from("Computer"),
+        board: [[0; SIZE]; SIZE],
+    };
+
+    println!("Player {}, with board {:?}", player1.name, player1.board);
+    println!("Player {}, with board {:?}", player2.name, player2.board);
+}
+
+struct Player {
+    name: String,
+    board: [[u32; SIZE]; SIZE],
+}
+
+fn attack(row: usize, col: usize, board: &mut [[u32; SIZE]; SIZE]) {
+    board[row][col] = 1;
 }

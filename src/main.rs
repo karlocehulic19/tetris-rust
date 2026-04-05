227b782c2fa6e9565a531cbbb038a2bfea53ef1e
Karlo Cehulic <karlocehlic@gmail.com>
2026-04-05 17:36:08 +0200

    feat: hello world using ratatui

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,19 @@
-mod game;
-use game::{Board, Player, ShipOrientation};
+use ratatui::{DefaultTerminal, Frame};
+fn main() -> color_eyre::Result<()> {
+    color_eyre::install()?;
+    ratatui::run(app)?;
+    Ok(())
+}
 
-fn main() {
-    let mut player1 = Player {
-        name: String::from("Joe"),
-        board: Board::new(),
-    };
-    let player2 = Player {
-        name: String::from("Computer"),
-        board: Board::new(),
-    };
-
-    let place1 = player1.place_ship(1, 1, 6, ShipOrientation::Vertical);
-    let place2 = player1.place_ship(2, 2, 3, ShipOrientation::Horizontal);
-    let place3 = player1.place_ship(1, 1, 3, ShipOrientation::Vertical);
-    match place1 {
-        Ok(()) => {
-            println!("NO!, should fail!!");
-        }
-        Err(msg) => {
-            println!("ERROR: {:?}", msg);
+fn app(terminal: &mut DefaultTerminal) -> std::io::Result<()> {
+    loop {
+        terminal.draw(render)?;
+        if crossterm::event::read()?.is_key_press() {
+            break Ok(());
         }
     }
-    // player1.place_ship(1, 1, 3, ShipOrientation::Vertical);
-    println!("Player {}, with board {:?}", player1.name, player1.board);
-    println!("Player {}, with board {:?}", player2.name, player2.board);
+}
+
+fn render(frame: &mut Frame) {
+    frame.render_widget("hello world", frame.area());
 }
